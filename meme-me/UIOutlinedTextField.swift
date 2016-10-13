e1a7dddfe15bf5891097e0dafa5b6699e51d806a
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-10-13 14:54:17 +0200

    add outlined textfield fx

---
+++ meme-me/UIOutlinedTextField.swift
@@ -0,0 +1,28 @@
+//
+//  UIOutlinedTextfield.swift
+//  meme-me
+//
+//  Created by Patrick Paechnatz on 13.10.16.
+//  Copyright © 2016 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UIOutlinedTextField: UITextField {
+    
+    var outlineWidth: CGFloat = 3
+    var outlineColor: UIColor = UIColor.black
+    
+    override func drawText(in rect: CGRect) {
+        
+        let strokeTextAttributes = [
+            NSStrokeColorAttributeName : outlineColor,
+            NSStrokeWidthAttributeName : -1 * outlineWidth,
+            ] as [String : Any]
+        
+        self.attributedText = NSAttributedString(string: self.text ?? "", attributes: strokeTextAttributes)
+        super.drawText(in: rect)
+    }
+}
+
