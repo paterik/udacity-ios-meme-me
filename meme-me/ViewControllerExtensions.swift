f4799aee2d0d86b86745981d56eb3de27e9e27b4
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-10-13 14:04:19 +0200

    add ViewController extension base

---
+++ meme-me/ViewControllerExtensions.swift
@@ -0,0 +1,44 @@
+//
+//  ViewControllerExtensions.swift
+//  meme-me
+//
+//  Created by Patrick Paechnatz on 13.10.16.
+//  Copyright © 2016 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension ViewController {
+
+    override func willRotate(to toInterfaceOrientation: UIInterfaceOrientation, duration: TimeInterval) {
+        
+        if (imagePickerSuccess) {
+            return
+        }
+        
+        if (toInterfaceOrientation.isLandscape) {
+            imagePickerView.image = UIImage(imageLiteralResourceName: "WelcomeLandscape")
+            imagePickerView.contentMode = .scaleAspectFit
+        } else {
+            imagePickerView.image = UIImage(imageLiteralResourceName: "WelcomePortrait")
+            imagePickerView.contentMode = .scaleAspectFit
+        }
+    }
+    
+    func isCameraAvailable() -> Bool {
+        return UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)
+    }
+    
+    func isPhotoLibrarayAvailable() -> Bool {
+        return UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary)
+    }
+    
+    func isSavedPhotosAlbumAvailable() -> Bool {
+        return UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.savedPhotosAlbum)
+    }
+    
+    func isLocalImageStockAvailable() -> Bool {
+        return isPhotoLibrarayAvailable() || isSavedPhotosAlbumAvailable()
+    }
+}
