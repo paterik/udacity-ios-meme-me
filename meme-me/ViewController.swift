c11ef53a29d9e12fe88eacf5f8ca34c354dd818d
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-10-14 15:06:54 +0200

    bound toolbar and exportButton controls, extend vc by exportImage and shareImage functions

--- meme-me/ViewController.swift
+++ meme-me/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var toolBarBottom: UIToolbar!
+    @IBOutlet weak var toolBarTop: UIToolbar!
+    
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var photoLibButton: UIBarButtonItem!
+    @IBOutlet weak var exportButton: UIBarButtonItem!
+    
     @IBOutlet weak var inputFieldTop: UIOutlinedTextField!
     @IBOutlet weak var inputFieldBottom: UIOutlinedTextField!
     
@@ -49,6 +54,39 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         super.viewWillDisappear(animated)
         unSubscribeToKeyboardNotifications()
+    }
+    
+    @IBAction func exportImage(_ sender: AnyObject) {
+        
+        // generate our memed image
+        let sourceImage: UIImage = renderMemedImage()
+        
+        // Create the subMenu controller (using alertViewController)
+        let alertController = UIAlertController(
+            title: "Export your image",
+            message: "Choose your export method",
+            preferredStyle: .alert)
+        
+        let normalExportAction = UIAlertAction(title: "save image", style: UIAlertActionStyle.default) {
+            UIAlertAction in
+            self.saveImage(renderedImage: sourceImage)
+        }
+        
+        let sharedExportAction = UIAlertAction(title: "share image", style: UIAlertActionStyle.default) {
+            UIAlertAction in
+            self.shareImage(renderedImage: sourceImage)
+        }
+        
+        // Add Basic actions
+        alertController.addAction(normalExportAction)
+        alertController.addAction(sharedExportAction)
+        
+        // Add Cancel action
+        alertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel) {
+            UIAlertAction in return
+        })
+        
+        self.present(alertController, animated: true, completion: nil)
     }
     
     @IBAction func pickCameraImage(_ sender: AnyObject) {
@@ -90,22 +128,38 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         // Add Cancel action
         alertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel) {
-            UIAlertAction in
-            
-            return
+            UIAlertAction in return
         })
         
         self.present(alertController, animated: true, completion: nil)
     }
     
-    func saveImage() {
-        
+    func saveImageModel(memedImage: UIImage) {
+    
+        // init/define our memed image model struct
         let _ = Meme(
             textTop: inputFieldTop.text!,
             textBottom: inputFieldBottom.text!,
             imageOrigin: imagePickerView.image!,
-            image: imagePickerView.image!
+            image: memedImage
         )
+    }
+    
+    func saveImage(renderedImage: UIImage) {
+        
+        saveImageModel(memedImage: renderedImage)
+        
+        // write incoming image to photo album
+        UIImageWriteToSavedPhotosAlbum(renderedImage, self, #selector(handleImageStorage(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    func shareImage(renderedImage: UIImage) {
+    
+        saveImageModel(memedImage: renderedImage)
+        
+        // open activitiy controller to share the incoming image
+        let activityViewController = UIActivityViewController(activityItems: [renderedImage as UIImage], applicationActivities: nil)
+        present(activityViewController, animated: true, completion: {})
     }
 }
 
