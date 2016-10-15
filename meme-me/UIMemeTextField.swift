8163c4d5e925f359f79c022d11ddbce3fa6723a8
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-10-15 12:11:56 +0200

    add UIMemeTextField class definition

---
+++ meme-me/UIMemeTextField.swift
@@ -0,0 +1,21 @@
+//
+//  UIMemeTextField.swift
+//  meme-me
+//
+//  Created by Patrick Paechnatz on 15.10.16.
+//  Copyright © 2016 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UIMemeTextField: UITextField {
+    
+    var contentDefault: String = ""
+    
+    func isChanged() -> Bool {
+
+        return self.text != contentDefault
+    }
+}
+
