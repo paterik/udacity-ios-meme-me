22773049867dbbc43b0596c9dc0f37d9ef924611
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-10-15 12:12:16 +0200

    add meme model definition as external resource

---
+++ meme-me/MemeModel.swift
@@ -0,0 +1,22 @@
+//
+//  MemeModel.swift
+//  meme-me
+//
+//  Created by Patrick Paechnatz on 15.10.16.
+//  Copyright © 2016 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//
+// MARK: Meme Model Definition
+//
+
+struct Meme {
+    
+    var textTop: String?
+    var textBottom: String?
+    var imageOrigin: UIImage?
+    var image: UIImage?
+}
