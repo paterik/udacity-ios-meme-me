8d8ec05a46f1030572fe48873fa4f37ddd7f61d3
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-10-18 14:19:28 +0200

    add CollectionView- and TableViewController

---
+++ meme-me/MemeCollectionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MemeCollectionViewController.swift
+//  meme-me
+//
+//  Created by Patrick Paechnatz on 18.10.16.
+//  Copyright © 2016 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeCollectionViewController: UICollectionViewController {
+
+}
