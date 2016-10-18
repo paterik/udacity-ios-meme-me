4c7781d9f8c442b6b583e674530df0dc083f9605
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-10-18 16:31:42 +0200

    add customized tableCell structure class

---
+++ meme-me/MemeTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MemeTableViewCell.swift
+//  meme-me
+//
+//  Created by Patrick Paechnatz on 18.10.16.
+//  Copyright © 2016 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var memeImage: UIImageView!
+    @IBOutlet weak var memeLabelTop: UILabel!
+    @IBOutlet weak var memeLabelBottom: UILabel!
+}
