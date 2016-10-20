a8afffb7fc5f5691781405882e9106be2d466df3
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-10-20 11:34:44 +0200

    add tableView controller extensions including first helper methods

--- meme-me/MemeTableViewControllerExtensions.swift
+++ meme-me/MemeTableViewControllerExtensions.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import UIKit
+
+extension MemeTableViewController {
+    
+    func initTableView() {
+    
+        if !isDataAvailable() {
+            
+            noDataImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: tableView.bounds.size.height))
+            noDataImageView.contentMode = .scaleAspectFit
+            noDataImageView.image = UIImage(imageLiteralResourceName: "WelcomeNoDataPortrait")
+            
+            tableView.backgroundView = noDataImageView
+            tableView.separatorStyle = .none
+            
+        } else {
+            
+            tableView.separatorStyle = .singleLine
+            tableView.backgroundView = nil
+        }
+    }
+    
+    func isDataAvailable() -> Bool {
+        return memes.count > 0
+    }
+}
