3517d51ccf51f5f582dfc8ccb72c1a59df60842c
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-10-20 13:37:41 +0200

    start working on collectionViewCell definition

---
+++ meme-me/MemeCollectionViewControllerExtension.swift
@@ -0,0 +1,9 @@
+//
+//  MemeCollectionViewControllerExtension.swift
+//  meme-me
+//
+//  Created by Patrick Paechnatz on 20.10.16.
+//  Copyright © 2016 Patrick Paechnatz. All rights reserved.
+//
+
+import Foundation
