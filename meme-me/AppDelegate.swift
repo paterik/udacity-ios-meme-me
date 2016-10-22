ef14aa105fd464fce8b528478b7726a90ee83417
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-10-22 14:35:05 +0200

    disable loadFixture() methodCall for review

--- meme-me/AppDelegate.swift
+++ meme-me/AppDelegate.swift
@@ -110,7 +110,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        loadFixtures()
+        // loadFixtures()
         
         return true
     }
