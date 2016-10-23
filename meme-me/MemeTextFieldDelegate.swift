f65c803f2f619e1634ec3ab2a50196a18850c69b
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-10-23 14:25:13 +0200

    add auto uppercase to fix strange iphone 6+ issue using storyboard-based auto-uppercase

--- meme-me/MemeTextFieldDelegate.swift
+++ meme-me/MemeTextFieldDelegate.swift
@@ -18,6 +18,15 @@ class MemeTextFieldDelegate : NSObject, UITextFieldDelegate {
         return true
     }
     
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if let text = textField.text {
+            textField.text = (text as NSString).replacingCharacters(in: range, with: string.uppercased())
+        }
+        
+        return false
+    }
+    
     func textFieldDidBeginEditing(_ textField: UITextField) {
         
         let memeTextField = textField as! UIMemeTextField
